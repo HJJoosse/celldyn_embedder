babfc8f3cc39e70c21c09a000691776d4aa21c2b
Huibert-Jan <h.j.joosse-3@umcutrecht.nl>
2022-08-17 15:18:04 +0200

    started quality metric + poincarre distance

--- quality_metrics.py
+++ quality_metrics.py
@@ -1,3 +1,35 @@
 # trustworthiness --> sklearn
 # distance correlation correlation --> zelf chefffen
 # knn-overlap - distance curve and integral
+# poincarre
+
+
+### knn_overlap -> find knn in true space random sample. Then embedding do the same for indices -> jaccard score
+
+import scipy as sc
+from sklearn.manifold import trustworthiness
+from sklearn.metrics import jaccard_score
+import dcor
+
+class CDEmbeddingPerformance:
+
+    def __init__(self,X_org:np.array,X_emb:np.array,metric:function):
+        self.X_org = X_org
+        self.X_emb = X_emb
+        self.metric = metric
+
+    def _return_trustworthiness(self):
+        return trustworthiness(self.X_org,self.X_emb)
+
+    def _return_knn_overlap(self):
+        
+        pass
+
+    def _distance_correlation(self):
+        dist_before = sc.spatial.distance.pdist(self.X_org,metric = self.metric)
+        dist_after = sc.spatial.distance.pdist(self.X_emb,metric = self.metric)
+
+        return dcor.distance_correlation(dist_before, dist_after)
+            
+    def score(self):
+        pass
---
+++ utils.py
@@ -0,0 +1,6 @@
+import jax.numpy as np
+poincarre_dist = lambda x,y: np.arccosh(\
+    1 + 2*(\
+        np.linalg.norm(x-y, ord=1)/((1-np.linalg.norm(x, ord=1))*(1-np.linalg.norm(y, ord=1)))
+        )
+    )

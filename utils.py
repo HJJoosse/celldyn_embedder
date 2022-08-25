3df3049cd1b97f0aeab597d4a39d332eda3dc2d6
Huibert-Jan <h.j.joosse-3@umcutrecht.nl>
2022-08-25 09:44:12 +0200

    update

--- utils.py
+++ utils.py
@@ -7,6 +7,6 @@ from numpy import linalg as la
 def poincarre_dist(x,y):
     return np.arccosh(\
     1 + 2*(\
-        la.norm(x-y, ord=1)/((1-la.norm(x, ord=1))*(1-la.norm(y, ord=1)))
+        la.norm(x-y, ord=1)**2/((1-la.norm(x, ord=1)**2)*(1-la.norm(y, ord=1)**2))
         )
     )
